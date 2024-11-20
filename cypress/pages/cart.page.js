8694cd184635a905e33b232725b1823625d29a93
Steeve BOKOSSA <steevebokossa@gmail.com>
2024-11-20 11:51:26 +0100

    getting the first item in the cart list

---
+++ cypress/pages/cart.page.js
@@ -0,0 +1,49 @@
+/// <reference types="cypress" />
+
+export class CartPage {
+  item = {
+    cartItems: () => cy.get(".cart_item"),
+  };
+
+  getFirstItemToCart() {
+    let itemData = {};
+    return this.item
+      .cartItems()
+      .first() // Récupère le premier élément
+      .then((firstItem) => {
+        // Utilisation de cy.wrap pour envelopper l'élément avant de l'utiliser avec .find()
+        cy.wrap(firstItem)
+          .find(".inventory_item_name")
+          .invoke("text")
+          .then((name) => {
+            itemData.name = name;
+
+            // Recherche de la description après avoir enveloppé firstItem avec cy.wrap
+            cy.wrap(firstItem)
+              .find(".inventory_item_desc")
+              .invoke("text")
+              .then((description) => {
+                itemData.description = description;
+
+                // Recherche du prix après avoir enveloppé firstItem avec cy.wrap
+                cy.wrap(firstItem)
+                  .find(".inventory_item_price")
+                  .invoke("text")
+                  .then((price) => {
+                    itemData.price = price;
+
+                    cy.wrap(firstItem)
+                      .find(".cart_quantity")
+                      .invoke("text")
+                      .then((quantity) => {
+                        itemData.quantity = quantity;
+
+                        return itemData;
+                      });
+                  });
+              });
+          });
+      });
+  }
+
+}
