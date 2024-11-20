ae5b0efbac7f69d1f9241065e26875af6d03a9a7
ayman hassnaoui <ayman12hassnaoui@gmail.com>
2024-11-20 16:17:01 +0100

    modifier 2  du  test de login

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter', // a ajouter
   e2e: {
+    chromeWebSecurity: false,
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);// a ajouter
     },
