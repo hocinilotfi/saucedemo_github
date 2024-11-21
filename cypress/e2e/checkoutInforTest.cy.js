fdcdb38aa2adcef93b212c7c449defadb57a0818
BlondelMbou <blondel.mbou@gmail.com>
2024-11-21 14:58:46 +0100

    checkout infor && checkout overview test

---
+++ cypress/e2e/checkoutInforTest.cy.js
@@ -0,0 +1,26 @@
+/// <reference types="cypress" />
+
+import CheckoutPage from "../pages/CheckoutPage";
+import PageLogin from "../pages/PageLogin";
+import Produit from "../pages/Produit";
+import panier from "../pages/RemoveInCart";
+
+describe("checkout overview test", () => {
+  const loginpage = new PageLogin();
+
+  const checkout = new CheckoutPage();
+
+  beforeEach(() => {
+    cy.visit("https://www.saucedemo.com/");
+  });
+  it("checkout overview test", () => {
+    loginpage.Login("standard_user", "secret_sauce");
+    cy.url().should("include", "/inventory.html");
+    Produit.boutonAjout();
+    panier.cliquePanier();
+    checkout.ButtonChekout();
+    checkout.fillCheckoutForm();
+    checkout.submitForm();
+    cy.url().should("include", "/checkout-step-two.html");
+  });
+});
