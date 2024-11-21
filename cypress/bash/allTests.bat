967c639161ef3469402a35e8dae3c69e235ebb0c
idir15 <nechab.idir15@gmail.com>
2024-11-21 15:53:15 +0100

    add test bash v02

--- cypress/bash/allTests.bat
+++ cypress/bash/allTests.bat
@@ -3,7 +3,7 @@
 echo --- Démarrage de l'exécution des tests ---
 
 :: Exécuter plusieurs fichiers de tests Cypress
-npx cypress run --spec "cypress/e2e/loginTest.cy.js,cypress/e2e/addCart.cy.js,cypress/e2e/return.Chekout.cy.js,cypress/e2e/testRemove.cy.js" --reporter mochawesome --reporter-options overwrite=false,html=true,json=false,reportDir=rapport
+npx cypress run --spec "cypress/e2e/loginTest.cy.js,cypress/e2e/addCart.cy.js,cypress/e2e/checkoutInforTest.cy.js,cypress/e2e/return.Chekout.cy.js,cypress/e2e/chekoutOverviewTest.cy.js,cypress/e2e/return.Order.cy.js,cypress/e2e/testRemove.cy.js" --reporter mochawesome --reporter-options overwrite=false,html=true,json=false,reportDir=rapport
 
 echo --- Tests terminés. Le rapport est généré dans le dossier 'rapport' ---
 pause
