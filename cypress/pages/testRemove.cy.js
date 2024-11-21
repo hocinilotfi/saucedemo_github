79626f9abab6e9df1d06661f4eec3c1479292589
Chahira BELHOCINE <belhocinechahira98@gmail.com>
2024-11-21 10:36:42 +0100

    Ajout du fichier de test pour la fonctionnalité Remove

---
+++ cypress/pages/testRemove.cy.js
@@ -0,0 +1,34 @@
+/// <reference types="cypress" />
+
+import PageLogin from "./PageLogin";
+import RemoveInCart from "./RemoveInCart";
+
+describe('Tests Saucedemo - Remove functionality', () => {
+    before(() => {
+        
+        cy.visit('https://www.saucedemo.com/');
+    });
+
+    it('Connexion à l\'application', () => {
+        
+        PageLogin.Login('standard_user', 'secret_sauce');
+
+    });
+
+    it('Test pour Remove', () => {
+        // Ajouter puis supprimer un produit
+        RemoveInCart.cliquerSac();
+        RemoveInCart.boutonAjout();
+        RemoveInCart.cliquePanier();
+        RemoveInCart.cliquerButtonRemove();
+
+      
+
+        cy.get('.cart_item').should('not.exist');
+
+        
+        cy.get('[data-test="continue-shopping"]').should('be.visible');
+        cy.get('[data-test="checkout"]').should('be.visible');         
+    });
+});
+
